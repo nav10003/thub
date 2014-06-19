c6ef0e3f64ac560d5ff614de0cb74eb55d61c10f
timothyjamesbecker <tbecker@d252h82.vpn.uconn.edu>
2014-06-19 17:50:44 -0400

    init commit

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 
-print "Content-type: text/html"
-print
-print "<html>"
-print "<head>"
-print "<title>CGI 101</title>"
-print "</head>"
-print "<body>"
-print "<h1>A First CGI Python example</h1>"
-print "<p>Hello CGI World!</p>"
-print "</body>"
-print "</html>"
+print ("Content-type: text/html")
+print ("<html>")
+print ("<head>")
+print ("<title>CGI 101</title>")
+print ("</head>")
+print ("<body>")
+print ("<h1>A First CGI Python example</h1>")
+print ("<p>Hello CGI World!</p>")
+print ("</body>")
+print ("</html>")
