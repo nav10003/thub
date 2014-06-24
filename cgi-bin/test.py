b6ccd0d4e9028838e3c48b012d5e0b819b0fa53a
timothyjamesbecker <tbecker@Bran.local>
2014-06-24 16:08:29 -0400

    html, css, js, dojo, cgi-python integrated!

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 
-print ("Content-type: text/html")
-print ("<html>")
-print ("<head>")
-print ("<title>CGI 101</title>")
-print ("</head>")
-print ("<body>")
-print ("<h1>A First CGI Python example</h1>")
-print ("<p>Hello CGI World!</p>")
-print ("</body>")
-print ("</html>")
+def print_each(text):
+    lines = text.split('\n')
+    for l in lines: print(l)
+    
+text = """
+Content-type: text/html
+<html>
+<head>
+<title>CGI 101</title>
+</head>
+<body>
+<h1>A First CGI Python example</h1>
+<p>Hello CGI World!</p>
+</body>
+</html>
+"""
+
+print_each(text)
