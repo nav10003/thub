b77c677f1909fdb08f257e755afc8b99b446a232
timothyjamesbecker <tbecker@d252h137.vpn.uconn.edu>
2014-06-27 15:17:02 -0400

    cgi testing

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -4,21 +4,21 @@ import cgitb
 
 cgitb.enable()
 
-def print_each(text):
-    lines = text.split('\n')
-    for l in lines: print l
-    
-text = """
-Content-type: text/html
+def printall(text):
+    text = text.split('\n')
+    for i in text: print i
+
+text="""Content-type: text/html
+
 <html>
-<head>
-<title>CGI 101</title>
-</head>
-<body>
-<h1>A First CGI Python example</h1>
-<p>Hello CGI World!</p>
-</body>
+  <head>
+    <title>CGI 101</title>
+  </head>
+  <body>
+  <h1>A First CGI Python example</h1>
+  <p>Hello CGI World!</p>
+  </body>
 </html>
 """
 
-print_each(text)
+printall(text)
