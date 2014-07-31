a9bdded22060290492452e7233fea16f852aa6a3
timothyjamesbecker <tbecker@d252h182.vpn.uconn.edu>
2014-07-31 17:43:53 -0400

    testing json to table functions

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -14,12 +14,15 @@ db_response = {}
 if uid=='test' and pwd=='db': #will replace with some basic user authentication mech
     with mssql.MSSQL('{SQL Server}','arc-gis','tHUB','dbread','tHUB2013@') as my_mssql:
         #test query, should return 100x3 table from ACS_T_Age
+        i,F = 0, {0:'geoid',1:'median',2:'total'}
         sql = "SELECT TOP 100 [geoid],[median],[total] FROM [ACS_T_Age] WHERE [acsyear]=2011"
         v = []
         response = my_mssql.query(sql,v,True)
         for r in response:
             l = list(r)
-            if len(l)>0 and not db_response.has_key(l[0]): db_response[l[0]]=l[1:]
+            if len(l)==len(F):
+                db_response[i] = {F[k]:l[k] for k in F}
+                i+=1
 
 print 'Content-type: application/json\n\n'
 print json.dumps(db_response, sort_keys=True,
