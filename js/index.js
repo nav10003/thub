66ade85f545a011578afee87963dba7452260934
timothyjamesbecker <tbecker@d252h194.vpn.uconn.edu>
2014-08-22 13:02:57 -0400

    a bunch of organizational constructs that are half baked

--- js/index.js
+++ js/index.js
@@ -6,64 +6,96 @@ require(["dijit/form/Button", "dojo/dom", "dojo/domReady!", "dojo/dom-construct"
   function(Button, dom, ready, con, request, domform, json, Grid, dojoNum){
     // Create a button programmatically:
     var myButton = new Button({
-        label: "test cgi",
+        label: "login",
         onClick: function(evt){ //attach an ajax POST call to the onclick event
-          con.place("<p>Requesting...</p>","result");
+          //con.place("<p>Loging in...</p>","result");
           // collect all the form values here
-          u = dom.byId("uid").value;
-          p = dom.byId("pwd").value;
+          var u = dom.byId("uid").value;
+          var p = dom.byId("pwd").value;
           // prevent the page from navigating after submit
           evt.stopPropagation();
           evt.preventDefault();
           // formulate the POST request and wait for the server response
-          request.post('cgi-bin/test.py',{ //the URL of the server side handler
+          request.post('cgi-bin/login.py',{ //the URL of the server side handler
             data: {uid:u, pwd: p}, // pack up the form as an object
             handleAs: 'json',      // sets the return data type (in this case you get back a JS Object)
             timeout: 2000          // Wait 2 seconds for a response
-          }).then(function(data){  // then is the area where you have an answer and can display results, etc
-            //example of what you can do after you get a table back...
-            con.place('<div id="grid"></div>',"result");  //make a new div called grid
-            
-            var table = []
-            //var fields = data[0];
-            var f = data[0];                             //get the field names
-            var fields = [];
-            fields.push('#');
-            for(e in f){ fields.push(f[e]); }
-            delete data[0];                              //delete for easy iteration
-            var i = 1;
-            for(e in data){
-                row = data[e];
-                row['#'] = i;
-                table.push(row);
-                i++; 
-            }        //fill the array with rows
-            
-            cols = {};
-            for(var i = 0; i < fields.length; i++){ cols[fields[i]] = fields[i]; }
-            console.log(fields);
-            var grid = new Grid({                                                 //dgrid example like excel
-              bufferRows: Infinity,                                               //have to construct
-              columns: cols
-              //columns: {                                                          //data as an array
-              //  "geoid":"geoid",                                                  //and specify the names
-              //  "median":{"label":"median","formatter":dojoNum.format},           //of the columns to display
-              //  "total":{"label":"total", "formatter":dojoNum.format}             //and format the data if needed
-              //}                                                                   //
-            },"grid");                                                            //attach to the grid div
-            grid.renderArray(table);                                              //load the data in from the array
-            
-            //this code constructs a .csv string from the table object
-            s = '';
-            for(e in table){ s+=String(table[e].geoid)+','+String(table[e].median)+','+String(table[e].total)+'\n'; }
-            //creates a client-side dynamic HTML5 download link with naming of the date using some jquery libs
-            dojo.byId("dl").innerHTML = '<a id="link" href="#">Download</a>';
-            $("a").on("click", function () {
-              var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
-              $(this).attr("href", "data:application/octet-stream," + encodeURIComponent(s)).attr("download", "file-" + d + ".csv");
-            });
-            //con.place("<p>response: <code>" + json.stringify(data) +"</code></p>","result"); //alternate place code
+          }).then(function(response){  // then is the area where you have an answer and can display results, etc
+            if(!("ERROR" in response)){   //no errors, login was good
+                con.empty("body");
+                con.place('<div id="result"></div>',"body");
+                con.place('<div id="dl"></div>',"body");
+                con.place('<div id="grid"></div>',"result");
+                data_grid(response,"grid");
+                download_csv(response,"dl");
+            }
+            else {                 //errors with login try again and log the IP
+                var result = dom.byId("result");
+                var text = response['ERROR']['text'];
+                var ip   = response['ERROR']['IP'];
+                result.innerHTML = '<br>'+text+'<br>'+'from IP adress: '+ip;
+            }
           });      
         }
-    }, "test").startup(); //start the button to finish attaching to the test div
+    }, "login").startup(); //start the button to finish attaching to the test div
 });
+
+function make_2D_array(data){
+  var table = [];
+  //var fields = data[0];
+  var f = data[0];                             //get the field names
+  var fields = [];
+  fields.push('#');
+  for(e in f){ fields.push(f[e]); }
+  //delete data[0];                              //delete for easy iteration
+  var i = 1;
+  for(e in data){
+    if(e>0){
+      row = data[e];
+      row['#'] = i;
+      table.push(row);
+      i++;
+    } 
+  }
+  return {'table':table,'fields':fields};
+}
+
+function data_grid(data,div){
+  require(["dgrid/Grid"], function(Grid){
+    var tf = make_2D_array(data);
+    var fields = tf['fields'];
+    var table  = tf['table'];       
+    cols = {};
+    for(var i = 0; i < fields.length; i++){ cols[fields[i]] = fields[i]; }
+    var grid = new Grid({                                                 //dgrid example like excel
+      bufferRows: Infinity,                                               //have to construct
+      columns: cols
+    },div);                                                            //attach to the grid div
+    grid.renderArray(table); 
+  });
+}
+
+//to do
+function download_csv(data,div){
+  require(["dojo/dom"], function(dom){
+    var tf = make_2D_array(data);
+    var fields = tf['fields'];
+    var table  = tf['table'];
+    //this code constructs a .csv string from the table object
+    s = '';
+    for(e in fields){ s+=String(fields[e])+','; }
+    s = s.slice(0,s.length-1); //take off last ','
+    s+='\n';
+    for(i in table){
+        for(j in fields){ s+=String(table[i][fields[j]])+','; }
+        s = s.slice(0,s.length-1);
+        s+='\n';
+    }
+    //creates a client-side dynamic HTML5 download link with naming of the date using some jquery libs
+    dom.byId(div).innerHTML = '<a id="link" href="#">Download</a>';
+    $("a").on("click", function () {
+        var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
+        $(this).attr("href", "data:application/octet-stream," + encodeURIComponent(s)).attr("download", "file-" + d + ".csv");
+    });
+  });
+}
---
+++ js/template.js
@@ -0,0 +1,69 @@
+//thub data form sending and table return handling tmeplate V1.0
+var s = ''; //global string used to make the .csv version of the dgrid table object
+require(["dijit/form/Button", "dojo/dom", "dojo/domReady!", "dojo/dom-construct",
+         "dojo/request","dojo/dom-form","dojo/json",
+         "dgrid/Grid","dojo/number"], 
+  function(Button, dom, ready, con, request, domform, json, Grid, dojoNum){
+    // Create a button programmatically:
+    var myButton = new Button({
+        label: "test cgi",
+        onClick: function(evt){ //attach an ajax POST call to the onclick event
+          con.place("<p>Requesting...</p>","result");
+          // collect all the form values here
+          u = dom.byId("uid").value;
+          p = dom.byId("pwd").value;
+          // prevent the page from navigating after submit
+          evt.stopPropagation();
+          evt.preventDefault();
+          // formulate the POST request and wait for the server response
+          request.post('cgi-bin/test.py',{ //the URL of the server side handler
+            data: {uid:u, pwd: p}, // pack up the form as an object
+            handleAs: 'json',      // sets the return data type (in this case you get back a JS Object)
+            timeout: 2000          // Wait 2 seconds for a response
+          }).then(function(data){  // then is the area where you have an answer and can display results, etc
+            //example of what you can do after you get a table back...
+            con.place('<div id="grid"></div>',"result");  //make a new div called grid
+            
+            var table = []
+            //var fields = data[0];
+            var f = data[0];                             //get the field names
+            var fields = [];
+            fields.push('#');
+            for(e in f){ fields.push(f[e]); }
+            delete data[0];                              //delete for easy iteration
+            var i = 1;
+            for(e in data){
+                row = data[e];
+                row['#'] = i;
+                table.push(row);
+                i++; 
+            }        //fill the array with rows
+            
+            cols = {};
+            for(var i = 0; i < fields.length; i++){ cols[fields[i]] = fields[i]; }
+            console.log(fields);
+            var grid = new Grid({                                                 //dgrid example like excel
+              bufferRows: Infinity,                                               //have to construct
+              columns: cols
+              //columns: {                                                          //data as an array
+              //  "geoid":"geoid",                                                  //and specify the names
+              //  "median":{"label":"median","formatter":dojoNum.format},           //of the columns to display
+              //  "total":{"label":"total", "formatter":dojoNum.format}             //and format the data if needed
+              //}                                                                   //
+            },"grid");                                                            //attach to the grid div
+            grid.renderArray(table);                                              //load the data in from the array
+            
+            //this code constructs a .csv string from the table object
+            s = '';
+            for(e in table){ s+=String(table[e].geoid)+','+String(table[e].median)+','+String(table[e].total)+'\n'; }
+            //creates a client-side dynamic HTML5 download link with naming of the date using some jquery libs
+            dojo.byId("dl").innerHTML = '<a id="link" href="#">Download</a>';
+            $("a").on("click", function () {
+              var d = new Date().toISOString().slice(0, 19).replace(/-/g, "");
+              $(this).attr("href", "data:application/octet-stream," + encodeURIComponent(s)).attr("download", "file-" + d + ".csv");
+            });
+            //con.place("<p>response: <code>" + json.stringify(data) +"</code></p>","result"); //alternate place code
+          });      
+        }
+    }, "test").startup(); //start the button to finish attaching to the test div
+});
